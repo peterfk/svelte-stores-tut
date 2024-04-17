60941b7d6152402abc6c1aed7d485418d406a936
Peter Kipping <pkipping@yahoo.com>
2024-04-17 16:28:55 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { count } from "./stores.js";
+  import { time } from "./stores.js";
+  import Incrementer from "./lib/Incrementer.svelte";
+  import Decrementer from "./lib/Decrementer.svelte";
+  import Resetter from "./lib/Resetter.svelte";
+
+  const formatter = new Intl.DateTimeFormat("en", {
+    hour12: true,
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+</script>
+
+<h1>The count is {$count}</h1>
+
+<Incrementer />
+<Decrementer />
+<Resetter />
+
+<h1>The time is {formatter.format($time)}</h1>
